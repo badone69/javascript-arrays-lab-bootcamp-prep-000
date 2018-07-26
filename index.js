0af8f0df6d543b07a1f2d0c02bc733272a6c7c46
student online <eliel.badon@gmail.com>
2018-07-26 20:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kitens = kittens.slice(0, kittens.length - 1);
+  kittens = kittens.slice(0, kittens.length - 1);
   return kittens
 }
